df5f623d2a325963f6072a15bd2018b332fade77
dimastrik-78 <78548398+dimastrik-78@users.noreply.github.com>
2023-10-10 14:53:42 +0300

    task2

--- LeoECS/Assets/_Source/Systems/EcsInitSystem.cs
+++ LeoECS/Assets/_Source/Systems/EcsInitSystem.cs
@@ -20,6 +20,17 @@ namespace Client
             foreach (int entity in _filter)
             {
                 ref EcsComponent testComponent = ref _entityPool.Get(entity);
+                ref var tra = ref testComponent.Anchor;
+                tra.position += tra.forward;
+                if (tra.position.x >= 1)
+                {
+                    tra.rotation = Quaternion.Euler(0, -60, 0);
+                }
+                else if (tra.position.x <= -1)
+                {
+                    tra.rotation = Quaternion.Euler(0, 60, 0);
+                }
+                
                 ref var floatCounter = ref testComponent.Counter;
                 floatCounter++;
                 Debug.Log(floatCounter);
